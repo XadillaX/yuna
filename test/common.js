0f06f0552fb886b9187dcc3085beded915343d96
XadillaX <admin@xcoder.in>
2015-05-18 20:52:52 +0800

    add connection unit tests

---
+++ test/common.js
@@ -0,0 +1,46 @@
+/**
+ * XadillaX created at 2015-05-18 15:18:03 With ♥
+ *
+ * Copyright (c) 2015 Huaban.com, all rights
+ * reserved.
+ */
+var Illyria = require("illyria");
+
+var ZK_CONNECTION_STRING = exports.ZK_CONNECTION_STRING = "127.0.0.1:2181";
+var ZK_ROOT = exports.ZK_ROOT = "/yuna_mocha";
+var ZK_PREFIX = exports.ZK_PREFIX = "/yuna_";
+var SERVER_HOST = exports.SERVER_HOST = "127.0.0.1";
+
+exports.createServer = function(port, callback) {
+    var server = Illyria.createServer({
+        port: port,
+        host: SERVER_HOST
+    }, {
+        connectString: ZK_CONNECTION_STRING,
+        root: ZK_ROOT,
+        prefix: ZK_PREFIX
+    });
+
+    server.expose("test", {
+        echo: function(req, resp) {
+            resp.send(req.params);
+        },
+
+        setTimeout: function(req, resp) {
+            var time = req.params.time;
+            setTimeout(function() {
+                resp.send(req.params);
+            }, time);
+        },
+
+        info: function(req, resp) {
+            resp.send({ port: port, host: SERVER_HOST });
+        }
+    });
+
+    server.listen(function(err) {
+        if(err) return callback(err);
+        callback(undefined, server);
+    });
+};
+
---
+++ test/connect.js
@@ -0,0 +1,113 @@
+/**
+ * XadillaX created at 2015-05-18 17:20:35 With ♥
+ *
+ * Copyright (c) 2015 Huaban.com, all rights
+ * reserved.
+ */
+var should = require("should");
+var Illyria = require("illyria");
+var Scarlet = require("scarlet-task");
+var common = require("./common");
+var Yuna = require("../");
+
+describe("connection test", function() {
+    var SERVER_COUNT = 10;
+    var SERVER_START_PORT = 17173;
+    var servers = [];
+    var yuna;
+
+    this.timeout(10000);
+
+    before(function(callback) {
+        var scarlet = new Scarlet(SERVER_COUNT);
+        var createServer = function(taskObject) {
+            var id = taskObject.task.id;
+            common.createServer(SERVER_START_PORT + id, function(err, server) {
+                should(err).not.be.instanceof(Error);
+                servers.push(server);
+
+                scarlet.taskDone(taskObject);
+            });
+        };
+
+        for(var i = 0; i < SERVER_COUNT; i++) {
+            scarlet.push({ id: i }, createServer);
+        }
+
+        scarlet.afterFinish(SERVER_COUNT, function() {
+            servers = servers.sort(function(a, b) {
+                return a.port - b.port;
+            });
+
+            callback();
+        }, false);
+    });
+
+    after(function() {
+        for(var i = 0; i < SERVER_COUNT; i++) servers[i].close();
+    });
+
+    it("should has 10 connections", function(callback) {
+        yuna = Yuna.createPool(common.ZK_CONNECTION_STRING, common.ZK_ROOT, common.ZK_PREFIX, {
+            maxPool: 10
+        });
+
+        var count = 0;
+        var ports = {};
+        var onNew = function(client) {
+            count++;
+            ports[client.port] = true;
+            if(count === 10) {
+                finish();
+            }
+        };
+        yuna.on("new", onNew);
+
+        var finish = function() {
+            yuna.pool.length.should.be.eql(10);
+            for(var i = 0; i < yuna.pool.length; i++) {
+                var client = yuna.pool.valueAt(i);
+                client.should.be.instanceof(Illyria.Client);
+                client.connectStatus().should.be.eql("CONNECTED");
+                ports[SERVER_START_PORT + i].should.be.eql(true);
+            }
+
+            yuna.removeListener("new", onNew);
+            callback();
+        };
+    });
+
+    it("should disconnect one and reconnect", function(callback) {
+        for(var i = 0; i < yuna.pool.length; i++) {
+            var client = yuna.pool.valueAt(i);
+            client.socket.socket.end();
+        }
+
+        var newCount = 0;
+        var closeCount = 0;
+        var onNew = function() {
+            newCount++;
+            if(newCount === 10 && closeCount === 10) finish();
+        };
+        var onClose = function() {
+            closeCount++;
+            if(newCount === 10 && closeCount === 10) finish();
+        };
+
+        yuna.on("new", onNew);
+        yuna.on("close", onClose);
+
+        var finish = function() {
+            for(var i = 0; i < yuna.pool.length; i++) {
+                var client = yuna.pool.valueAt(i);
+                client.should.be.instanceof(Illyria.Client);
+                client.connectStatus().should.be.eql("CONNECTED");
+            }
+
+            yuna.removeListener("new", onNew);
+            yuna.removeListener("close", onClose);
+            callback();
+        };
+    });
+});
+
